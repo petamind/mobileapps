727f3ea643e886f6186281bd7f635bc400901e23
tung.nguyen <doantung.nguyen@animationcollege.co.nz>
2018-08-06 13:21:27 +1200

    hello animation

--- mobileapps/android/games/TheFlyingDragon/app/src/main/java/com/example/tungnguyen/theflyingdragon/MainActivity.java
+++ mobileapps/android/games/TheFlyingDragon/app/src/main/java/com/example/tungnguyen/theflyingdragon/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.tungnguyen.theflyingdragon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus){
+            Animation animation = AnimationUtils.loadAnimation(this, R.anim.sun_rise);
+            findViewById(R.id.imageView).startAnimation(animation);
+            Animation animation1 = AnimationUtils.loadAnimation(this, R.anim.clock_turn);
+            findViewById(R.id.imageView3).startAnimation(animation1);
+        }
     }
 }
