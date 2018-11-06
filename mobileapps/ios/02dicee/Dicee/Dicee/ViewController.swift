cc26dc6a3e6d1529342b2f3aa03df98a71d915c3
Tung Nguyen Nguyen <093178@que385093178mac.local>
2018-11-06 15:57:15 +1300

    finish dicee

--- mobileapps/ios/02dicee/Dicee/Dicee/ViewController.swift
+++ mobileapps/ios/02dicee/Dicee/Dicee/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Dicee
+//  Dicee: simple dice with random
 //
 //  Created by Tung Nguyen Doan on 4/11/18.
 //  Copyright © 2018 Tung Nguyen Doan. All rights reserved.
@@ -14,17 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var dice2: UIImageView!
     
     @IBAction func rollemBtn(_ sender: Any) {
+        updateDiceFace()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceFace()
+    }
+    
+    func updateDiceFace(){
         let n1 = Int.random(in: 1..<7)
         let n2 = Int.random(in: 1..<7)
         dice1.image = UIImage(named: "dice\(n1)")
         dice2.image = UIImage(named: "dice\(n2)")
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
 
 }
 
